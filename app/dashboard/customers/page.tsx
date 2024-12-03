902d0533c6805b07af807db137dc1d9058886a81
Soyeon Lee <pink731p@gmail.com>
2024-12-03 15:33:05 +0900

    feat: metadata 제목 추가

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+   title: 'Customers',
+};
+
 export default function Page() {
-    return <p>Customers Page</p>;
+   return (
+      <>
+         <h1>Customers Page</h1>
+      </>
+   );
 }
